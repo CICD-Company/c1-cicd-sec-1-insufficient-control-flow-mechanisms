1b6b0523a13cbbdb86032748737df584e7966561
CICD-Company <jacopo.stucchi1@studenti.unimi.it>
2026-01-09 17:57:15 +0100

    Refactor after attacker hack Risk CICD-SEC-1 - Vulnerability Shared library abuse

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def main():
     print("2 + 3 =", add(2, 3))
 
     # Function of the shared library
-    log("Calculator started")
+    log("Calculator started (after hack to utility library)")
 
 
 if __name__ == "__main__":
