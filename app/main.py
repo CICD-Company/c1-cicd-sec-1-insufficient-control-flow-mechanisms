8c545a2208d2033549afb167ac049e9f9b6293bc
CICD-Company <jacopo.stucchi1@studenti.unimi.it>
2026-01-09 18:24:13 +0100

    Update main Risk CICD-SEC-1 - Vulnerability Shared library abuse

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ from utils import log
 def main():
     print("2 + 3 =", add(2, 3))
 
-    # Function of the shared library
-    log("Calculator started")
+    # Function of the shared library ([HACK])
+    log("Calculator started after Utility-library hack")
 
 
 if __name__ == "__main__":
